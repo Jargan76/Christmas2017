916385f3034a6a3c57c0751f1554db468db27101
Eric Hebel <Jargan76Studio@gmail.com>
2017-12-19 10:43:11 -0500

    message update
    added more messages

--- messageConditions.js
+++ messageConditions.js
@@ -5,9 +5,14 @@ function messageConditions(){
         text("Have a ball", 125, 150);
         text("This Holiday Season!",74,175);
     }
-    if(countDwn <= -8){
+    if(countDwn <= -8 && countDwn > -13){
         fill(33)
         text("I'm glad you are enjoying my game.", 70, 150);
         text("Please share with your friends.",70,175);
+    }
+    if(countDwn <= -15 ){
+        fill(33)
+        text("Wow you are dedicated!", 70, 150);
+        text("This is the last message!",70,175);
     }
 }
